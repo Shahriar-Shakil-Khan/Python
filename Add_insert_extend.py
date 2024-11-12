14c231fde621bcce640856ce19c5afa87b1b1ec4
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-11-12 19:47:49 +0600

    Add files via upload

---
+++ Add_insert_extend.py
@@ -0,0 +1,14 @@
+fruit=["Apple","Banana","Orange","Guava"]
+fruit.append("Mango")
+print(fruit)
+
+fruit1=["Apple","Banana","Orange","Guava"]
+fruit1.insert(3,"Mango")
+print(fruit1)
+
+fruit2=["Apple","Banana"]
+fruit3=["Orange","Guava"]
+fruit2.extend(fruit3)
+print(fruit2)
+
+
