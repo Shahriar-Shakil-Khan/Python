59f938ba2bf23afc098e9856f4f4c94d378c6f56
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-11-13 21:00:46 +0600

    Add files via upload

---
+++ immutable_float.py
@@ -0,0 +1,7 @@
+x=10.50
+first_location=id(x)
+print(first_location)
+
+x=20.33
+second_location=id(x)
+print(second_location)
---
+++ immutable_frozenset.py
@@ -0,0 +1,11 @@
+x=frozenset([1,2,3,4,5])
+first_location=id(x)
+print(x)
+print(type(x))
+print(first_location)
+
+x=frozenset([1,2,3,4,5])
+second_location=id(x)
+print(x)
+print(type(x))
+print(second_location)
