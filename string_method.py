f45ecebf800bb4f9ac2d1ebd121531a0dde9e2c0
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-11-17 18:47:02 +0600

    Add files via upload

---
+++ string_method.py
@@ -0,0 +1,21 @@
+name4='shahriar shakil'
+x4=name4.swapcase()
+print(x4)
+
+name='shahriar shakil'
+x=name.upper()
+print(x)
+
+name1='SHAHRIAR SHAKIL'
+x1=name1.lower()
+print(x1)
+
+name3='zakaria nabil'
+x3=name3.capitalize()
+print(x3)
+
+name5='Hello , World!'
+print(name5)
+y=name5.replace("Hello","Hi")
+print(y)
+
