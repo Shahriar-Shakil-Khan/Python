0521a1962dd6b6d8e40bbefe53bc5dae13cb6382
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-11-19 20:02:51 +0600

    Add files via upload

---
+++ Python Function, File Handling & Error Handling/default_update_function.py
@@ -0,0 +1,6 @@
+def add_two_number(a,b=60):
+    summation=a+b
+    print("summation : ",summation)
+
+add_two_number(400,500)
+# here pera meter is a 40 but b is 50 but b will be updated and value is 50.
