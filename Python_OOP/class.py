8a8866f40d6b8b00ac2e45c8ed386ffbcad4389e
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-02-20 23:53:04 +0600

    Add files via upload

---
+++ Python_OOP/class.py
@@ -0,0 +1,23 @@
+
+'''
+obj1=Myclass()
+print(obj1.x)
+
+obj2=Myclass()
+print(obj2.y)
+
+obj3=Myclass()
+print(obj3.z)
+'''
+
+class Myclass:
+    x=10
+    y=20
+    z=30
+    sum=x+y+z
+
+obj3=Myclass()
+print(obj3.x)
+print(obj3.y)
+print(obj3.z)
+print(obj3.sum)
