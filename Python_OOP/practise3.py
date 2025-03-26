5befd9516f52f333c519cedd29005262294d11ce
Shahriar   Shakil <shahriarshakil28@gmail.com>
2025-03-26 17:39:08 +0600

    Add files via upload

---
+++ Python_OOP/practise3.py
@@ -0,0 +1,19 @@
+
+class Myclass:
+
+    def __init__(self):
+        print("I am Constructor")
+
+obj=Myclass()
+
+
+class Myclass:
+
+    x=10
+    y=90
+
+    def __init__(self):
+        sum=self.x+self.y
+        print(sum)
+
+obj=Myclass()
