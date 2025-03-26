2f98a67aa42918673f36c471464c60fe07c5856f
Shahriar   Shakil <shahriarshakil28@gmail.com>
2025-03-26 17:38:08 +0600

    Add files via upload

---
+++ Python_OOP/practise2.2.py
@@ -0,0 +1,24 @@
+class Myclass:
+    a=10
+    b=20
+    c=70
+
+    def addSum(self,x,y):
+        summation=self.a+self.b++self.c + x + y
+        print(summation)
+
+    def addSub(self):
+        self.addSum(5,5)
+
+
+
+obj=Myclass()
+obj.addSum(3,2)
+
+obj.addSub()
+
+
+
+
+
+
